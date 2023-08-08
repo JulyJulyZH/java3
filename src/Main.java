caa1bd7c75780ddfc113c411ea3449d207cbf2ea
Zharova Yulya <zharovayulya1997@gmail.com>
2023-08-08 20:31:26 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int refill = 1100;
+        int bonus = 0;
+        if (refill > 1000)
+        {bonus = refill/100;}
+        int total = account + refill + bonus;
+        System.out.println(total);
+    }
+}
