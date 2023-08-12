087e224d10959dde0a490cc15c02ed6ddced1004
Zharova Yulya <zharovayulya1997@gmail.com>
2023-08-12 14:18:23 +0500

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main {
         int account = 100;
         int refill = 1100;
         int bonus = 0;
-        if (refill > 1000)
-        {bonus = refill/100;}
+        if (refill > 1000) bonus = refill / 100;
         int total = account + refill + bonus;
         System.out.println(total);
     }
